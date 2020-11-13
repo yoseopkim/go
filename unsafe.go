40617f9058b7d67ffc7df7ccd53a2f95743d3725
yoseopkim <yosopkim@icloud.com>
2020-11-14 01:51:22 +0900

    first commit

---
+++ unsafe.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "fmt"
+    "unsafe"
+)
+
+func main() {
+   var value int64 = 5
+   var p1 = &value
+   var p2 = (*int32)(unsafe.Pointer(p1))
+
+
+   fmt.Println("*p1: ", *p1)
+   fmt.Println("*p2: ", *p2)
+   *p1 = 423234234235323
+   fmt.Println(value)
+   fmt.Println("*p2: ", *p2)
+   *p1 = 423231424
+   fmt.Println(value)
+   fmt.Println("*p2: ", *p2)
+}
